9bfd83a8f2ec3a235f4b96bd99812d15fc8c7575
jclarino <jclarino@savvysherpa.com>
2018-05-07 09:24:52 +0800

    test db switch

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 
-var production = true;
+var production = false;
 
 var prod_uri = "mongodb://treasurerapp:1234@ds163769.mlab.com:63769/treasurerapp";
 var test_uri = "mongodb://treasurerapp:1234@ds113640.mlab.com:13640/treasurerapp-test";
